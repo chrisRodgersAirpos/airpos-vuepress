fbd806db78d50f1dd6e676d3ba9f9f360107d08f
Chris Rodgers <chrisrogers@airpos.co.uk>
2021-02-12 10:58:36 +0000

    base url fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://vuepress.local',
+    baseURL: 'https://vuepress.cjrodgers.co.uk/',
   },
 
 
