3324717afc042f56f70db8a44ca2d75eaca173b1
Chris Rodgers <chrisrogers@airpos.co.uk>
2021-02-12 14:31:06 +0000

    testimonial block

---
+++ components/TestimonialBlock.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div
+        v-for="(testimonial, id) in homeContent.acf.testimonial_tiles"
+        :key="id"
+        class="col-lg-4"
+      >
+        <h3>{{ testimonial.post_title }}</h3>
+        <p>{{ testimonial.post_content }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['homeContent']),
+  },
+
+  created() {
+    this.getHomeContent()
+  },
+
+  methods: {
+    ...mapActions(['getHomeContent']),
+  },
+}
+</script>
