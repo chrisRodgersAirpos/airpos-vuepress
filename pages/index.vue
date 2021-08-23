ace1e78186e0c51d464639c81f7ad21be6190c51
Chris Rodgers <chrisrogers@airpos.co.uk>
2021-08-23 13:46:48 +0100

    Example deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <div class="container">
-      <p>This is the Homepage</p>
+      <p>This is the Homepage.</p>
     </div>
     <FeatureBlock />
-    <ReviewWidget />
     <TestimonialBlock />
   </div>
 </template>
